b19e392cef6a4c632f6fff53b7094d99dc7fa1f1
Artem Glushenko <artemgl9999@gmail.com>
2022-06-27 12:44:40 +0300

    create socials list block

---
+++ src/components/SocialsList/SocialsList.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import links from "./links"
+
+const SocialsList = () => {
+    return (
+        <nav>
+            {links.map((IconLink) => <Link to="{IconLink.url}">{IconLink.svg}</Link>)}
+        </nav>
+    )
+}
+
+export default SocialsList
---
+++ src/components/SocialsList/links.ts
@@ -0,0 +1,17 @@
+type IconLink = {
+    url: string;
+    svg: string;
+};
+
+const links: Array<IconLink> = [
+    {
+        url: "https://instagram.com",
+        svg: "instagramAlt"
+    },
+    {
+        url: "https://facebook.com",
+        svg: "facebookAlt"
+    }
+];
+
+export default links;
