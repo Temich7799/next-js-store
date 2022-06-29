b74b86d3bea37e8c07cf0005400cac40fd5934c2
Artem Glushenko <artemgl9999@gmail.com>
2022-06-29 13:15:03 +0300

    remove plugin-google-fonts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,18 +14,7 @@ const config: GatsbyConfig = {
     options: {
       "trackingId": "none"
     }
-  },
-  {
-    resolve: `gatsby-plugin-google-fonts`,
-    options: {
-      fonts: [
-        `Comfortaa`,
-        //`source sans pro\:300,400,400i,700` // you can also specify font weights and styles
-      ],
-      display: 'swap'
-    }
-  }
-    , "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
