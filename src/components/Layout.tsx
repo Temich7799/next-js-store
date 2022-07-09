42baf07aae7892fd998737a54abeee9694c34553
Artem Glushenko <artemgl9999@gmail.com>
2022-07-09 14:11:36 +0300

    get content data with query

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,31 @@
-import * as React from "react"
+import * as React from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import { graphql } from "gatsby";
 
-interface IProps {
-    children: JSX.Element;
-}
-
-const Layout = (props: IProps) => {
+const Layout = ({ data }: any) => {
+    console.log(data);
     return (
         <>
             <Header />
-            {props.children}
+            <main>
+                <h1>{data.wpPage.title}</h1>
+                {(data.wpPage.content) ? data.wpPage.content : '404'}
+            </main>
             <Footer />
         </>
     )
 }
 
 export default Layout
+
+export const query = graphql`
+query ($slug: String!) {
+    wpPage(slug: {eq: $slug}) {
+      content
+      title
+    }
+  } 
+`
+
+
