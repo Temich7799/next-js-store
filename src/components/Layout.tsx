b0c4a17812f7646355b47659be8bdb536c6285e2
Artem Glushenko <artemgl9999@gmail.com>
2022-07-12 16:15:33 +0300

    pretify

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,14 @@ import Footer from "./Footer/Footer";
 import { graphql } from "gatsby";
 
 const Layout = ({ data }: any) => {
-    console.log(data);
-    return (
-        <>
-            <Header />
-            {(data.wpPage.content) ? <main dangerouslySetInnerHTML={{ __html: data.wpPage.content }} /> : '404'}
-            <Footer />
-        </>
-    )
+  console.log(data);
+  return (
+    <>
+      <Header />
+      {(data.wpPage.content) ? <main dangerouslySetInnerHTML={{ __html: data.wpPage.content }} /> : '404'}
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
