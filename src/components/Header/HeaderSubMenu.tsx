5c7be1eab53c933cb8b15f30d7fb1611ad18c71c
Artem Glushenko <artemgl9999@gmail.com>
2022-07-13 14:46:43 +0300

    add styles

--- src/components/Header/HeaderSubMenu.tsx
+++ src/components/Header/HeaderSubMenu.tsx
@@ -3,9 +3,13 @@ import * as React from "react"
 import styled from "styled-components"
 
 const StyledHeaderSubMenu = styled.nav`
+position: absolute;
+display: flex;
+flex-direction: column;
 width: 300px;
 min-height: 50px;
-background-color: blue;
+font-weight: normal !important;
+background-color: rgba(5,5,5,0.25);
 z-index: 1000;
 `;
 
