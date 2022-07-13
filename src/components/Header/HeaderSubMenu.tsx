0ecb43c2dbe0ceea0b67c0e41bb6f70e58125fb2
Artem Glushenko <artemgl9999@gmail.com>
2022-07-13 11:23:16 +0300

    create component for subMenu

---
+++ src/components/Header/HeaderSubMenu.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const StyledHeaderSubMenu = styled.nav`
+min-height: 50px;
+background-color: blue;
+z-index: 1000;
+`;
+
+const HeaderSubMenu = () => {
+
+    return (
+        <StyledHeaderSubMenu>
+
+        </StyledHeaderSubMenu>
+    )
+}
+
+export default HeaderSubMenu;
