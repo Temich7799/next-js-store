782646e0a3da49dc75b87c2b8d19538afa3d50cf
Artem Glushenko <artemgl9999@gmail.com>
2022-07-13 11:24:22 +0300

    add subMenu component

--- src/components/Header/HeaderMenu.tsx
+++ src/components/Header/HeaderMenu.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { StaticImage } from "gatsby-plugin-image"
+import HeaderSubMenu from "./HeaderSubMenu";
 
 const StyledMenu = styled.nav`
 min-width: 700px;
@@ -20,31 +21,39 @@ const Menu = () => {
 
   const data = useStaticQuery(graphql`
     query HeaderMenu {
-      wpMenu(id: {eq: "dGVybTozMQ=="}) {
-        menuItems {
-          nodes {
-            label
-            url
-          }
+    wpMenu(id: {eq: "dGVybTozMQ=="}) {
+      menuItems {
+        nodes {
+          label
+          url
         }
       }
-    } 
+    }
+  }
+
   `);
 
   const links = data.wpMenu.menuItems.nodes;
-  console.log(links);
+  console.log(data)
+
   return (
     <StyledMenu>
-      {links.map((link: any) => (links.indexOf(link) == Math.floor(links.length / 2))
-        ?
-        <>
-          <Link to="https://home">
-            <StaticImage src="../../images/logo.png" alt="Logo" placeholder="blurred" layout="fixed" width={100} height={100} />
-          </Link>
-          <Link to={link.url}>{link.label}</Link>
-        </>
-        :
-        <Link to={link.url}>{link.label}</Link>)}
+      {
+        links.map((link: any) => (links.indexOf(link) == Math.floor(links.length / 2))
+          ?
+          <>
+            <Link to="https://home">
+              <StaticImage src="../../images/logo.png" alt="Logo" placeholder="blurred" layout="fixed" width={100} height={100} />
+            </Link>
+            <Link to={link.url}>{link.label}</Link>
+            {(link.childItems.nodes.length) ? <HeaderSubMenu /> : false}
+          </>
+          :
+          <>
+            <Link to={link.url}>{link.label}</Link>
+            {(link.childItems.nodes.length) ? <HeaderSubMenu /> : false}
+          </>)
+      }
     </StyledMenu>
   )
 }
