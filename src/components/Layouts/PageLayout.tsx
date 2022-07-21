242222bdc398c6963cf63a8770efcc9aa0ff14e8
Artem Glushenko <artemgl9999@gmail.com>
2022-07-21 13:02:46 +0300

    add wp styles

--- src/components/Layouts/PageLayout.tsx
+++ src/components/Layouts/PageLayout.tsx
@@ -2,6 +2,16 @@ import * as React from "react";
 import Layout from "./MainLayout";
 import { graphql } from "gatsby";
 import PageTitle from "../PageTitle";
+import styled from "styled-components";
+require('../../styles/wp.css');
+
+const Main = styled.main`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 50px;
+    padding: 2.5%;
+`;
 
 type PageProps = {
   data: {
@@ -21,7 +31,9 @@ const PageLayout = (props: PageProps) => {
       <Layout>
         <>
           <PageTitle>{data.wpPage.title}</PageTitle>
-          {(data.wpPage.content) ? <main dangerouslySetInnerHTML={{ __html: data.wpPage.content }} /> : '404'}
+          <Main>
+            {(data.wpPage.content) ? <main dangerouslySetInnerHTML={{ __html: data.wpPage.content }} /> : '404'}
+          </Main>
         </>
       </Layout>
     </>
