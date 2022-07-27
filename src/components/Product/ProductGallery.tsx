1a85743e24be3fdaa844971083b96d5f508ff25d
Artem Glushenko <artemgl9999@gmail.com>
2022-07-27 20:03:51 +0300

    make product gallery

--- src/components/Product/ProductGallery.tsx
+++ src/components/Product/ProductGallery.tsx
@@ -1,11 +1,48 @@
-import * as React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 
 const StyledProductGallery = styled.div`
-    width: 500px;
+    width: 400px;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ProductGallerySelectedImage = styled.img`
+    width:300px;
     height: 400px;
-    img {
-        height: 100%;
+`;
+
+const ProductGalleryCarousel = styled.div`
+    width: 100px;
+    height: 400px;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    row-gap: 5px;
+    overflow: scroll;
+`;
+
+type ProductGalleryCarouselImageProps = {
+    isSelected?: boolean
+}
+
+const ProductGalleryCarouselImage = styled.img<ProductGalleryCarouselImageProps>`
+    width: 100px;
+    height: 100px;
+    padding-left: 5px;
+    object-fit: cover;
+    ${(props) => {
+        switch (props.isSelected) {
+            case true:
+                return `
+                filter: grayscale(85%);
+            `;
+            default:
+                return `
+                cursor: pointer;
+            `;
+        }
+    }
     }
 `;
 
@@ -22,9 +59,22 @@ const ProductGallery = (props: ProductGalleryProps) => {
 
     const { data } = props;
 
+    const [selectedImage, setSelectedImage] = useState(0);
+
     return (
         <StyledProductGallery>
-            <img src={data[0].src} alt={data[0].alt}></img>
+            <ProductGallerySelectedImage src={data[selectedImage].src} alt={data[selectedImage].alt} />
+            <ProductGalleryCarousel>
+                {
+                    data.map((img) =>
+                        < ProductGalleryCarouselImage
+                            isSelected={data.indexOf(img) == selectedImage && true}
+                            src={img.src}
+                            alt={img.alt}
+                            onClick={(e) => setSelectedImage(data.indexOf(img))}
+                        />)
+                }
+            </ProductGalleryCarousel>
         </StyledProductGallery>
     )
 }
