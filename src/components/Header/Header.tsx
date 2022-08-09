0402886ad58f9540f7bc4994a9b7c29cd9d91640
Artem Glushenko <artemgl9999@gmail.com>
2022-08-09 12:32:38 +0300

    align items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,22 +4,23 @@ import Menu from "./HeaderMenu"
 import styled from "styled-components"
 
 const StyledHeader = styled.header`
-font-family: 'Amatic SC';
-font-size: 24px;
-font-weight: 100;
-line-height: 27px;
-color: #585858;
-a {
-    color: #585858; 
-    text-decoration: none;
-}
-height: 140px;
-width: calc(100% - 86px);
-padding: 12px 43px;
-display: grid;
-justify-items: center;
-grid-template-columns: 1fr 4fr 1fr;
-box-shadow: -1px 4px 5px -2px rgba(0,0,0,0.25);
+    font-family: 'Amatic SC';
+    font-size: 24px;
+    font-weight: 100;
+    line-height: 27px;
+    color: #585858;
+    a {
+        color: #585858; 
+        text-decoration: none;
+    }
+    height: 140px;
+    width: calc(100% - 86px);
+    padding: 12px 43px;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 1fr 4fr 1fr;
+    box-shadow: -1px 4px 5px -2px rgba(0,0,0,0.25);
 `;
 
 const Header = () => {
