a125f71feacf069756d7a6e6a1ce6978808f7e4a
Artem Glushenko <artemgl9999@gmail.com>
2022-09-03 02:22:26 +0300

    fix

--- src/services/hooks/useMobile.ts
+++ src/services/hooks/useMobile.ts
@@ -4,15 +4,10 @@ export default function useMobile(mobileWidth: number = 820): boolean {
     const [isMobile, setIsMobile] = useState<boolean>(false);
 
     useEffect(() => {
-
-        window.addEventListener('resize', onResizeHandler);
-
-        return () => window.removeEventListener('resize', onResizeHandler);
-
-        function onResizeHandler(): void {
-            setIsMobile(window.innerWidth < mobileWidth ? true : false);
-        }
-
+        window.addEventListener('resize', () => setIsMobile(window.innerWidth < mobileWidth ? true : false));
+        setIsMobile(window.innerWidth < mobileWidth ? true : false);
+        
+        return () => window.removeEventListener('resize', () => setIsMobile(window.innerWidth < mobileWidth ? true : false));
     }, []);
 
     return isMobile;
