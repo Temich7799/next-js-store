5f7f5394e2fa65d1f54691043aa062357a166c98
Artem Glushenko <artemgl9999@gmail.com>
2022-09-14 23:55:07 +0300

    remove ReactImageMagnify (due to conflicts)

--- src/components/Products/ProductGallery/ProductGallerySelectedImage.tsx
+++ src/components/Products/ProductGallery/ProductGallerySelectedImage.tsx
@@ -1,8 +1,7 @@
 import * as React from "react"
-import ReactImageMagnify from "react-image-magnify";
 import styled from "styled-components"
 
-const StyledProductGallerySelectedImage = styled.div`
+const StyledProductGallerySelectedImage = styled.img`
     width:300px;
     height: 400px;
 `;
@@ -22,25 +21,7 @@ const ProductGallerySelectedImage = (props: ProductGallerySelectedImageProps) =>
     const { selectedImage, data } = props;
 
     return (
-        <StyledProductGallerySelectedImage>
-            <ReactImageMagnify {...{
-                smallImage: {
-                    alt: data[selectedImage].alt,
-                    src: data[selectedImage].src,
-                    isFluidWidth: true,
-                    width: 200,
-                    height: 200
-                },
-                largeImage: {
-                    src: data[selectedImage].src,
-                    alt: data[selectedImage].alt,
-                    width: 900,
-                    height: 1200
-                },
-                enlargedImagePosition: 'over',
-                hoverDelayInMs: 50
-            }} />
-        </StyledProductGallerySelectedImage>
+        <StyledProductGallerySelectedImage src={data[selectedImage].src} alt={data[selectedImage].alt} />
     )
 }
 
