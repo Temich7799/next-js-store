5b102f2cbd63fa280ac3c7b1b257cc1d5d5bd883
Artem Glushenko <artemgl9999@gmail.com>
2022-09-23 20:13:10 +0300

    minor fixes

--- express/index.js
+++ express/index.js
@@ -1,24 +1,20 @@
 
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const schema = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 
 const port = process.env.PORT;
 
-app.use(cors({ origin: true, credentials: true }));
-
-app.use('/graphql', graphqlHTTP({
+app.use('/', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
     graphiql: true,
 }));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Started server on port ${port}`);
 });
