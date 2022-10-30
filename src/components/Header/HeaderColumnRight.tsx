284cb52de0edfa0fde6f99fec804fde454da23df
Artem Glushenko <artemgl9999@gmail.com>
2022-10-31 00:04:31 +0200

    add gap

--- src/components/Header/HeaderColumnRight.tsx
+++ src/components/Header/HeaderColumnRight.tsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 const StyledHeaderColumnRight = styled.div`
     display: flex;
     align-items: center;
+    gap: 15px;
 `;
 
 const HeaderColumnRight = () => {
