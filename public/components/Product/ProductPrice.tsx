96b1150f9c3f3f8f38ede41d15927444f7f9a352
Temich7799 <artemgl9999@gmail.com>
2023-01-30 13:23:10 +0300

    fixes for a6361b3: add isPriceLoading

--- public/components/Product/ProductPrice.tsx
+++ public/components/Product/ProductPrice.tsx
@@ -1,11 +1,13 @@
 import React, { useContext } from "react"
 import styled from "styled-components"
 import { PageContext } from "../../templates/BaseTemplate";
+import LoadingSpinner from "../LoadingBars/LoadingSpinner";
 
 type ProductPriceProps = {
     price: string | undefined
     salePrice: string | undefined | null
     showTitle?: boolean
+    isPriceLoading?: boolean
 }
 
 const StyledProductPrice = styled.div<any>`
@@ -36,7 +38,7 @@ const ProductPrice = (props: ProductPriceProps) => {
     const { language } = useContext(PageContext);
     const { PRODUCT_PRICE_TITLE, PRODUCT_PRICE_EMPTY, CURRENCY } = require(`../../languages/${language}/languages`);
 
-    let { price, salePrice, showTitle = true } = props;
+    let { price, salePrice, showTitle = true, isPriceLoading = false } = props;
 
     const emptyPrice = price === '' ? PRODUCT_PRICE_EMPTY : undefined;
 
@@ -46,10 +48,12 @@ const ProductPrice = (props: ProductPriceProps) => {
                 showTitle && <p>{`${PRODUCT_PRICE_TITLE}:`}</p>
             }
             {
-                salePrice && salePrice.length > 0
-                    ? <p><OldPrice>{!emptyPrice ? price : emptyPrice}{" "}</OldPrice><SalePrice>{" "}{salePrice}</SalePrice> {!emptyPrice && CURRENCY}</p>
-                    : <p>{!emptyPrice ? price : emptyPrice} {!emptyPrice && CURRENCY}</p>
-
+                isPriceLoading
+                    ? <LoadingSpinner size="15%" />
+                    :
+                    salePrice && salePrice.length > 0
+                        ? <p><OldPrice>{!emptyPrice ? price : emptyPrice}{" "}</OldPrice><SalePrice>{" "}{salePrice}</SalePrice> {!emptyPrice && CURRENCY}</p>
+                        : <p>{!emptyPrice ? price : emptyPrice} {!emptyPrice && CURRENCY}</p>
             }
         </StyledProductPrice>
     )
